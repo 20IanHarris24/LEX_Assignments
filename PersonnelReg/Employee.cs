eb33c9caf9d856cfb3bf81cd85e443687a400ca9
ian_h <ian_h@HERMES-WINDOWS2>
2024-05-20 16:56:46 +0200

    Add project files.

---
+++ PersonnelReg/Employee.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonnelReg
+{
+    internal class Employee
+    {
+
+        public string firstname = "";
+        public string surname = "";
+        public int salary;
+
+
+        
+        
+        public Employee(string firstname, string surname, int salary)
+        {
+            this.firstname = firstname;
+            this.surname = surname;
+            this.salary = salary;
+
+
+            DisplayEmployeeDetails(firstname, surname, salary );
+
+
+        } 
+       
+              
+        
+        
+        
+       
+        private void DisplayEmployeeDetails(string firstname, string surname, int salary)
+        {
+            Console.WriteLine("\nname: " + firstname + "\nSurname: " + surname + "\nSalary: " + salary);
+        }
+
+
+    }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
---
+++ PersonnelReg/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace PersonnelReg
+{
+
+    /*     Qu1. Employee class
+     *     Qu2. Attributes: Firstname, Surname, Salary. Methods for printing
+     */
+
+
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            string firstname = "";
+
+
+            Employee person = new Employee();
+
+
+            Console.Write("Enter Employee firstname: ");
+            person.firstname = Console.ReadLine();
+            Console.Write("\n");
+            Console.Write("Enter Employee surname: ");
+            person.surname = Console.Readline();
+            Console.Write("\n");
+            Console.Write("Enter Employee salary: ");
+            person.salary = int.Parse(Console.Readline());
+
+           
+
+          
+
+        }
+    }
+}
